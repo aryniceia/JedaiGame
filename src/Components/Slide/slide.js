5404e07b9c17e292f29e48184d594910fbff0f1f
Aryane Nicéia <aryniceia@gmail.com>
2021-11-10 22:37:28 -0300

    Finalização do slideshow

--- src/Components/Slide/slide.js
+++ src/Components/Slide/slide.js
@@ -16,35 +16,34 @@ import Tucano from '../../img/char/tucano.png';
 
 const delay = 10000;
 const items = [
-  
   {id: 1, titleCapi: '🧪 Capi', idadeCapi: '🧬 Idade: 28 anos', alturaCapi: '📏 Altura: 1,65m', pesoCapi: '⚖️ Peso: 70kg', 
-          descricao: '📜 Bio: Dona de diversos apelidos: Carpincha, \n Beque, Trombuda, Cachapu, Cunum \n e Cubu, ela está sempre comendo \n alguma coisa no laboratório, a \n despeito das inúmeras broncas que \n já tomou do Prof. Dr. Mamutão. Calma e \n prestativa, prefere realizar seus \n experimentos à tarde. É a veterana que \n já devia ter se formado, mas que \n ninguém quer que vá embora. \n Todes precisam de sua ajuda para \n alguma coisa.', 
-          src: Capivara, colors:'#FF5E8C'},
+          descricaoCapi: '📜 Bio: Dona de diversos apelidos: Carpin- \n cha, Beque, Trombuda, Cachapu, Cunum e \n Cubu, ela está sempre comendo \n alguma coisa no laboratório, a \n despeito das inúmeras broncas que \n já tomou do Prof. Dr. Mamutão. \n Calma e prestativa, prefere \n realizar seus experimentos à \n tarde. É a veterana que já devia ter \n se formado, mas que ninguém quer \n que vá embora. Todes precisam de \n sua ajuda para alguma coisa.', 
+          src1: Capivara, colors: 'RGBA(211,141,181,0.97)'},
 
   {id: 2, title: '🧪 Guará', idade:'🧬 Idade: 20 anos', altura:'📏 Altura: 1,90m ', peso:'⚖️ Peso: 80kg', 
-          descricao:'📜 Bio: Figura excêntrica, sempre de casaco, \n capuz e fones de ouvido. Fica quieto na \n dele, curtindo o frio polar do ar \n condicionado, enquanto pacientemente \n purifica recursivamente todos os reagentes \n para seus experimentos. Jamais toque em \n suas vidrarias, meticulosamente limpas \n e guardadas de um jeito que só ele \n consegue.', 
-          src: Guara, colors:'#00C49F'},
+          descricao:'📜 Bio: Figura excêntrica, sempre de \n casaco, capuz e fones de ouvido. \n Fica quieto na dele, curtindo o frio \n polar do ar condicionado, enquanto \n pacientemente purifica \n recursivamente todos os reagentes \n para seus experimentos. Jamais toque \n em suas vidrarias, meticulosamente \n limpas e guardadas de um jeito \n que só ele consegue.', 
+          src2: Guara, colors: 'RGBA(211,141,181,0.97)'},
 
-  {id: 4, title: '🧪 Prof. Dr. Mamutão', idade:'🧬 Idade: desconhecida, dizem habitar esse \n laboratório desde o plioceno.', 
-          altura:'📏 Altura: 2,01m ', peso:'⚖️ Peso: 127 kg ',
-          descricao:'📜 Bio: Apesar da cara sisuda, \n adora uma brincadeira e qualquer \n coisa é motivo para festa. \n Geralmente encontrado em seu \n habitat natural: o laboratório de \n química orgânica em horários não \n convencionais, quando pode realizar \n seus experimentos malucos sem o \n escrutínio des alunes.',
-          src: Mamute, colors: '#FF6846'},
+  {id: 4, titleMamute: '🧪 Prof. Dr. Mamutão', idadeMamute:'🧬 Idade: desconhecida, dizem habitar esse laboratório \n desde o plioceno.', 
+          alturaMamute:'📏 Altura: 2,01m ', pesoMamute:'⚖️ Peso: 127 kg ',
+          descricaoMamute:'📜 Bio: Apesar da cara sisuda, \n adora uma brincadeira e \n qualquer coisa é motivo \n para festa. Geralmente \n encontrado em seu \n habitat natural: o laboratório \n de química orgânica em \n horários não convencionais, \n quando pode realizar \n seus experimentos malucos sem \n o escrutínio des alunes.',
+          src: Mamute, colors: 'RGBA(211,141,181,0.97)'},
 
   {id: 5, title: '🧪 Miquito', idade:'🧬 Idade: 21 anos', altura:'📏 Altura: 1,60m', peso:'⚖️ Peso: 55kg ', 
-          descricao:'📜 Bio: Não se conhece alguém que \n seja mais rápido para montar uma \n coluna de separação ou limpar o \n rotaevaporador. Pena que quase sempre \n tem que recomeçar seus experimentos, \n porque esqueceu algum reagente ou de \n acertar algum parâmetro.',
-          src: Mico, colors: '#6B638C'},  
+          descricao:'📜 Bio: Não se conhece alguém \n que seja mais rápido para montar \n uma coluna de separação ou limpar \n o rotaevaporador. Pena que quase \n sempre tem que recomeçar seus \n experimentos, porque esqueceu \n algum reagente ou de acertar \n algum parâmetro.',
+          src5: Mico, colors: 'RGBA(211,141,181,0.97)'},  
 
   {id: 6, title: '🧪 Pintada', idade:'🧬 Idade: 19 anos', altura:'📏 Altura: 1,70m ', peso:'⚖️ Peso: 60kg ',
-          descricao:'📜 Bio: É a mais nova aluna do laboratório. \n Ainda não sabe muito bem o que fazer da \n vida, então se dedica a várias entidades, \n à atlética e começou recentemente \n uma IC. Com tanta coisa para fazer e \n uma graduação exigente, quase nunca \n está no laboratório, mas dizem que leva \n jeito para coisa, porque tudo o que \n toca dá certo.',
-          src: Onca, colors: '#0088FE'},
+          descricao:'📜 Bio: É a mais nova aluna do \n laboratório. Ainda não sabe muito \n bem o que fazer da vida, então se \n dedica a várias entidades, à \n atlética e começou recentemente \n uma IC. Com tanta coisa para fazer \n e uma graduação exigente, quase \n nunca está no laboratório, mas \n dizem que leva jeito para coisa,\n porque tudo o que toca dá certo.',
+          src: Onca, colors: 'RGBA(211,141,181,0.97)'},
 
   {id: 7, title: '🧪 Tuca', idade:'🧬 Idade: 24 anos', altura:'📏 Altura: 1,68m', peso:'⚖️ Peso: 55kg ',
-          descricao:'📜 Bio: Figura constante no \n laboratório. Até parece que não \n dorme. Não sabe onde está um \n reagente ou equipamento? É só \n perguntar para o Tuca, ele não só vai \n pegar para você como contar sobre tudo \n que tem acontecido no laboratório.',
-          src: Tucano, colors: '#FFBB28'},
+          descricao:'📜 Bio: Figura constante no \n laboratório. Até parece que \n não dorme. Não sabe onde \n está um reagente ou equi- \n -pamento? É só perguntar \n para o Tuca, ele não só vai \n pegar para você como contar \n sobre tudo que tem acontecido \n no laboratório.',
+          src: Tucano, colors: 'RGBA(211,141,181,0.97)'},
 
   {id: 3, title: '🧪 Cuca', idade:'🧬 Idade: 23 anos', altura:'📏 Altura: 1,75m ', peso:'⚖️ Peso: 69kg',
-          descricao:'📜 Bio: Sempre com um livro na mão e uma \n curiosidade científica bem aleatória para \n compartilhar. É a ela que você \n recorre quando o \n Prof. Dr. Mamutão não está por \n perto para tirar a sua dúvida. Não vê \n a hora de terminar a graduação para \n poder passar mais tempo no \n laboratório.',
-          src: Jacare, colors: '#FF4430'},
+          descricao:'📜 Bio: Sempre com um \n livro  na mão e uma curio- \n sidade científica bem \n  aleatória para compartilhar. \n  É a ela que você recorre quando \n o Prof. Dr. Mamutão não está \n por perto para tirar a sua dúvida. \n Não vê a hora de terminar a \n  graduação para poder passar \n  mais tempo no laboratório.',
+          src3: Jacare, colors: 'RGBA(211,141,181,0.97)'},
 ];
 
 function Slideshow() {
@@ -56,7 +55,6 @@ function Slideshow() {
       clearTimeout(timeoutRef.current);
     }
   }
-
   React.useEffect(() => {
     resetTimeout();
     timeoutRef.current = setTimeout(
@@ -72,51 +70,66 @@ function Slideshow() {
   }, [index]);
 
   return (
-    <div className="slideshow">
-      <div
-        className="slideshowSlider"
-        style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
-      >
-        {items.map((backgroundColor, index) => (
-          <div
-            className="slide"
-            key={index}
-            style={{ backgroundColor: backgroundColor.colors }}
-          >
-              <img className="image-char" src={backgroundColor.src} />
+    <Link>
+      <div className="slideshow">
+        <div
+          className="slideshowSlider"
+          style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+        >
+          {items.map((backgroundColor, index) => (
+            <div
+              className="slide"
+              key={index}
+              style={{ backgroundColor: backgroundColor.colors }}
+            >
+                <img className="image-char" src={backgroundColor.src} />
+                <img className="imageCapi" src={backgroundColor.src1} /> {/*Foto apenas da capivara */}
+                <img className="imageGuara" src={backgroundColor.src2} /> {/*Foto apenas da lobo guará*/}
+                <img className="imageJacare" title1="Vacina, gostoso demais!" src={backgroundColor.src3} /> {/*Foto apenas do jacaré*/}
+                <img className="imageMico" src={backgroundColor.src5} /> {/*Foto apenas do mico*/}
 
-              <p className="titleCapi">{backgroundColor.titleCapi}</p>
-              <p className="ageCapi">{backgroundColor.idadeCapi}</p>
-              <p className="heighCapi">{backgroundColor.alturaCapi}</p>
-              <p className="weightCapi">{backgroundColor.pesoCapi}</p>
+                {/*Informações específicas*/}
+                  {/*Capivara*/}
+                    <p className="titleCapi">{backgroundColor.titleCapi}</p>
+                    <p className="ageCapi">{backgroundColor.idadeCapi}</p>
+                    <p className="heighCapi">{backgroundColor.alturaCapi}</p>
+                    <p className="weightCapi">{backgroundColor.pesoCapi}</p>
+                    <p className="bioCapi">{backgroundColor.descricaoCapi}</p>
+                  {/*Mamute*/}
+                    <p className="titleMamute">{backgroundColor.titleMamute}</p>
+                    <p className="ageMamute">{backgroundColor.idadeMamute}</p>
+                    <p className="heighMamute">{backgroundColor.alturaMamute}</p>
+                    <p className="weightMamute">{backgroundColor.pesoMamute}</p>
+                    <p className="bioMamute">{backgroundColor.descricaoMamute}</p>
 
-              <p className="textInfo">{backgroundColor.title}</p>
-              <p className="age">{backgroundColor.idade}</p>
-              <p className="heigh">{backgroundColor.altura}</p>
-              <p className="weight">{backgroundColor.peso}</p>
-              <p className="bio">{backgroundColor.descricao}</p>
-          
-          </div>
-        ))}
-    </div>
-
-      <div className="slideshowDots">
-        {items.map((_, idx) => (
-          <div
-            key={idx}
-            className={`slideshowDot${index === idx ? " active" : ""}`}
-            onClick={() => {
-              setIndex(idx);
-            }}
-          ></div>
-        ))}
+                {/*Informações gerais*/}
+                  <p className="textInfo">{backgroundColor.title}</p>
+                  <p className="age">{backgroundColor.idade}</p>
+                  <p className="heigh">{backgroundColor.altura}</p>
+                  <p className="weight">{backgroundColor.peso}</p>
+                  <p className="bio">{backgroundColor.descricao}</p>
+            </div>
+          ))}
       </div>
-    </div>
+
+        <div className="slideshowDots">
+          {items.map((_, idx) => (
+            <div
+              key={idx}
+              className={`slideshowDot${index === idx ? " active" : ""}`}
+              onClick={() => {
+                setIndex(idx);
+              }}
+            ></div>
+          ))}
+        </div>
+      </div>
+      
+      <Link route="/" className='volta'>
+        <img alt="Botão de seta para voltar" className="left-one" title="Voltar" src={Arrow}></img>
+      </Link>
+    </Link>
   );
 }
-
-<Link route="/" className='volta'>
-    <img alt="Botão de seta para voltar" className="left" title="Voltar" src={Arrow}></img>
-</Link>
 
 export default Slideshow;
