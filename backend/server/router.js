cbe5024b60846c942daaacdd02a05cc1772b0d5a
Vitalson Lorran Viana Carvalhal <vitalson_lorran95@ymail.com>
2022-02-09 12:53:28 -0300

    atualizar backend

---
+++ backend/server/router.js
@@ -0,0 +1,9 @@
+//just some middleware
+const express = require("express");
+const router = express.Router();
+//basic express shit
+router.get("/", (req, res) => {
+  res.send({ response: "Server is up and running." }).status(200);
+});
+
+module.exports = router;
